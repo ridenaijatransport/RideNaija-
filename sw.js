0085a43a0e466f208ac223aa113e1ec9f872b5fa
ridenaijatransport <ridenaijatransport@gmail.com>
2026-06-22 04:51:09 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ridenaija-v19';
+const CACHE_NAME = 'ridenaija-v20';
 const ASSETS = ['/', '/index.html', '/admin.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
